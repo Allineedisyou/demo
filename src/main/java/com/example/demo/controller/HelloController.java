5abc785eeab4fcaee8861968ba62a3df235c5ae2
wind19996 <wanghao1491@163.com>
2019-04-04 11:20:45 +0800

    修改一下

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -5,7 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloController {
-
+    /**
+     * 输出hello world
+     * @param args
+     */
     @RequestMapping("/hello")
     public static void main(String[] args){
         System.out.println("Hello World");
